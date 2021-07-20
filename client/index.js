9feb0ec2b3fa18edd442f240775ec23e9416b0a1
JulissaDantes <julissa@singularsound.com>
2021-07-20 18:34:04 -0400

    Thrid version

--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
 import "./index.scss";
 
 const server = "http://localhost:3042";
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
+const ec1 = new EC('secp256k1');
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
   if(value === "") {
@@ -19,9 +22,18 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = document.getElementById("private-key").value;
 
+  const transaction = {
+    amount, recipient, sender, privateKey
+  }
+
+  const key = ec1.keyFromPrivate(privateKey, 'hex');
+	console.log("is the first key defined:",key);
+  const signature = key.sign(SHA256(JSON.stringify(transaction)).toString());
+console.log("is the first sig defined:",signature);
   const body = JSON.stringify({
-    sender, amount, recipient
+    transaction, signature:signature.toDER('hex'), privateKey: privateKey
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
--- server/index.js
+++ server/index.js
@@ -2,31 +2,80 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const EC = require('elliptic').ec;
+const ec = new EC('curve25519');
+const ec1 = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+class Account{
+	constructor(keyPair, balance){
+	this.accountName = keyPair.getPublic().encode('hex').toString().substr(24)
+	this.publicKey = keyPair.getPublic().encode('hex').toString()
+	this.privateKey = keyPair.getPrivate().toString('hex')
+	this.balance = balance
+	}
+}
+let runnning = false;
+const accounts = []
+accounts.push(new Account(ec.genKeyPair(),100))
+accounts.push(new Account(ec.genKeyPair(),50))
+accounts.push(new Account(ec.genKeyPair(),75))
+
+function printInfo(){
+	console.log("Available accounts: \n =================" )
+	for(let account of accounts){
+		console.log("(",accounts.indexOf(account),") ", account.accountName," (",account.balance,"ETH)")
+	}
+	console.log("Private Keys:\n =================")
+	for(let account of accounts){
+		console.log("(",accounts.indexOf(account),")", account.privateKey)
+	}
 }
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
-  const balance = balances[address] || 0;
+  const crrtAccount = accounts.find(acc => acc.accountName.toLowerCase() == address.toLowerCase()) || 0;
+  const balance = crrtAccount.balance;
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+console.log('This is middleware', req.originalUrl);
+	if(!runnning){
+		runnning= true
+	  const {transaction, signature, privateKey} = req.body;
+	  const senderAccount = accounts.find(acc => acc.accountName.toLowerCase() == transaction.sender.toLowerCase());
+	  const recipientAccount = accounts.find(acc => acc.accountName.toLowerCase() == transaction.recipient.toLowerCase());
+
+	 //Verifies user has the right to transfer funds 
+     const key = ec1.keyFromPrivate(req.body.key.priv, 'hex');
+	 console.log("Am i getting a defined key:", key, 'full body',req.body.key.priv);
+	 const hash = SHA256(JSON.stringify(transaction)).toString();
+	 	 console.log("I exploded after this ln and this is my key",key,'and public is ',key.getPublic().encode('hex'));
+	  if(key.verify(hash, signature)){
+		   //removes from sender
+		   senderAccount.balance -= Number(transaction.amount);
+		   //adds amount to recipient	 
+		   console.log("public receipient:",recipientAccount.accountName);	   
+		   recipientAccount.balance += Number(transaction.amount);
+	  }else{
+		  throw new Error('This shall not pass')
+	  }
+	  printInfo();
+	  runnning = false
+	  res.send( {balance: senderAccount.balance} );
+	}else{
+		  res.send( {balance: 0} );
+	}
+
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  printInfo();
 });
